0eb4fdbffb00fc9cc101aff47ab876aa10657107
RaduTek <me@radutek.uk>
2026-07-14 23:47:09 +0300

    Flags: Add helper, and use flag to select preferred version

---
+++ helpers/flags.py
@@ -0,0 +1,59 @@
+from typing import TypedDict
+from flask import request
+
+
+COOKIE_PREFIX = 'backtube_'
+
+class Flags(TypedDict):
+    """User experience settings flags."""
+    
+    preferred_version: str
+
+
+DEFAULT_FLAGS = Flags(
+    preferred_version='2012'
+)
+
+def get_flags() -> Flags:
+    """Return the user's experience settings flags from their cookies."""
+    
+    flags = {}
+    flags.update(DEFAULT_FLAGS)
+
+    # Load flags from cookies
+    for key in flags.keys():
+        cookie_name = COOKIE_PREFIX + key
+        cookie_value = request.cookies.get(cookie_name)
+        if cookie_value is not None:
+            flags[key] = cookie_value
+    
+    return Flags(**flags)
+
+
+def get_flag(key: str, default: str = '') -> str:
+    """Return the value of a specific flag."""
+
+    cookie_name = COOKIE_PREFIX + key
+    cookie_value = request.cookies.get(cookie_name)
+
+    if cookie_value is not None:
+        return cookie_value
+    
+    return DEFAULT_FLAGS.get(key, default)
+
+
+def get_flag_bool(key: str, default: bool = False) -> bool:
+    """Return the boolean value of a specific flag."""
+
+    value = get_flag(key, str(default).lower())
+    return value.lower() in ['1', 'true', 'yes', 'on']
+
+
+def get_flag_int(key: str, default: int = 0) -> int:
+    """Return the integer value of a specific flag."""
+
+    value = get_flag(key, str(default))
+    try:
+        return int(value)
+    except ValueError:
+        return default
--- pages/__init__.py
+++ pages/__init__.py
@@ -1,9 +1,9 @@
-
+from ..helpers.flags import get_flag
 
 def get_preferred_template(template: str) -> str:
     """Return the user's preferred template"""
 
     # Here the preferred version should be retrieved
-    preferred_version = 2012
+    preferred_version = get_flag('preferred_version')
 
     return f"/{preferred_version}/{template}.html.j2"
