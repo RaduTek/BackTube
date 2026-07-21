07dbe9fd77f32d2ad331e6b7d4a286e86706741a
RaduTek <me@radutek.uk>
2026-07-21 22:23:38 +0300

    Config: New vars and utility function

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 import os
 from dataclasses import dataclass
 
+from utils import is_boolean_string
+
 
 @dataclass(frozen=True, slots=True)
 class Config:
+    debug: bool = is_boolean_string(os.getenv('BACKTUBE_DEBUG', 'False'))
     cache_dir: str = os.getenv('BACKTUBE_CACHE_DIR', 'cache')
+    enable_proxy: bool = is_boolean_string(os.getenv('BACKTUBE_ENABLE_PROXY', 'True'))
+    allowed_proxy_hosts: str = os.getenv('BACKTUBE_ALLOWED_PROXY_HOSTS', 'youtube.com,ytimg.com,example.com')
 
 
 config = Config()
--- utils.py
+++ utils.py
@@ -15,3 +15,7 @@ def find_nested_key(adict: dict, key: str, default=None) -> Optional[dict]:
                 stack += v
     
     return default
+
+
+def is_boolean_string(value: str) -> bool:
+    return value.lower() in ['1', 'true', 'yes', 'on']
